d39f3550a11850ec477e6ce739f59fae00b1860c
Gerard-Gon <gera.gonzalezs@duocuc.cl>
2026-05-09 19:49:27 -0400

    hotfix:frontend no llegaba a la base de datos

--- src/main/java/innovatech/apigateway/config/SecurityConfig.java
+++ src/main/java/innovatech/apigateway/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         
         //Se elimino la "/" al final de localhost:5173 para que coincida exactamente con el navegador
         //Aca al subir el frontend a vercel agregar el URL para que permita interactuar con el API
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "https://innovatech-front-end.vercel.app/","https://innovatechfrontend.onrender.com")); 
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "https://innovatech-front-end.vercel.app","https://innovatechfrontend.onrender.com")); 
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         
         //Permitimos el Token de Firebase y datos en JSON
